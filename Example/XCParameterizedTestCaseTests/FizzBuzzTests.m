b01d5b441e2e30178b4106639d025ff5d7a3725c
Michal Konturek <michal.konturek@gmail.com>
2013-12-08 13:02:03 +0000

    .

--- Example/XCParameterizedTestCaseTests/FizzBuzzTests.m
+++ Example/XCParameterizedTestCaseTests/FizzBuzzTests.m
@@ -32,29 +32,4 @@
     XCTAssertEqualObjects(self.expected, result, @"");
 }
 
-//- (void)test_01_returns_01 {
-//    NSInteger input = 1;
-//    assertThat([FizzBuzz outputForInteger:input], equalTo(@"1"));
-//}
-//
-//- (void)test_03_returns_fizz {
-//    NSInteger input = 3;
-//    assertThat([FizzBuzz outputForInteger:input], equalTo(@"Fizz"));
-//}
-//
-//- (void)test_05_returns_buzz {
-//    NSInteger input = 5;
-//    assertThat([FizzBuzz outputForInteger:input], equalTo(@"Buzz"));
-//}
-//
-//- (void)test_06_returns_fizz {
-//    NSInteger input = 6;
-//    assertThat([FizzBuzz outputForInteger:input], equalTo(@"Fizz"));
-//}
-//
-//- (void)test_15_returns_fizzbuzz {
-//    NSInteger input = 15;
-//    assertThat([FizzBuzz outputForInteger:input], equalTo(@"FizzBuzz"));
-//}
-
 @end
